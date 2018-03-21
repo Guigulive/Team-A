ec76ad3997847b10ea9e5e5d8ea9bd856d74faa7
CUI DI <cuidi.co@gmail.com>
2018-03-21 22:33:08 +0900

    Assignment 3

--- Lesson-3/assignment/README.md
+++ Lesson-3/assignment/README.md
@@ -3,7 +3,17 @@
 
 ### 第三课：课后作业
 - 第一题：完成今天所开发的合约产品化内容，使用Remix调用每一个函数，提交函数调用截图
+在images文件夹里面
 - 第二题：增加 changePaymentAddress 函数，更改员工的薪水支付地址，思考一下能否使用modifier整合某个功能
+在yours.sol里添加了
+```
+function changePaymentAddress (address oldEmployeeId, address newEmployeeId) onlyOwner employeeExist(oldEmployeeId) employeeNotExist(newEmployeeId) public {
+    var oldEmployee = employees[oldEmployeeId];
+    _partialPaid (oldEmployee);
+    employees[newEmployeeId] = Employee(newEmployeeId, SafeMath.mul(oldEmployee.salary, 1 ether), now);
+    delete employees[oldEmployeeId];
+}
+```
 - 第三题（加分题）：自学C3 Linearization, 求以下 contract Z 的继承线
 - contract O
 - contract A is O
--- Lesson-3/assignment/yours.sol
+++ Lesson-3/assignment/yours.sol
@@ -1,1 +1,101 @@
 /*作业请提交在这个目录下*/
+pragma solidity ^0.4.14;
+
+import "./SafeMath.sol";
+
+contract Payroll {
+    struct Employee {
+        address id;
+        uint salary;
+        uint lastPayday;
+    }
+
+    uint constant PAY_DURATION = 10 seconds;
+
+    address owner;
+    uint totalSalary = 0;
+
+    mapping(address => Employee) public employees;
+
+
+    modifier employeeExist(address employeeId) {
+        var employee = employees[employeeId];
+        assert(employee.id != 0x0);
+        _;
+    }
+
+    modifier employeeNotExist(address employeeId) {
+        var employee = employees[employeeId];
+        assert (employee.id == 0x0);
+        _;
+    }
+
+    modifier onlyOwner {
+        require (msg.sender == owner);
+        _;
+    }
+
+    function Payroll() payable public {
+        owner = msg.sender;
+    }
+
+    function _partialPaid(Employee employee) private {
+        uint payment = SafeMath.mul(employee.salary, SafeMath.div(SafeMath.sub(now, employee.lastPayday), PAY_DURATION));
+        employee.id.transfer(payment);
+        employee.lastPayday = now;
+    }
+
+    function addEmployee(address employeeId, uint salary) onlyOwner employeeNotExist(employeeId)  public {
+        totalSalary = SafeMath.add(totalSalary, SafeMath.mul(salary, 1 ether));
+        employees[employeeId] = Employee(employeeId, SafeMath.mul(salary, 1 ether), now);
+    }
+
+    function removeEmployee(address employeeId) onlyOwner employeeExist(employeeId) public {
+        var employee = employees[employeeId];
+        _partialPaid(employee);
+        delete employees[employeeId];
+        totalSalary = SafeMath.sub (totalSalary, employee.salary);
+    }
+
+    function updateEmployee(address employeeId, uint salary) onlyOwner public {
+        var employee = employees[employeeId];
+        _partialPaid(employee);
+        totalSalary = SafeMath.sub (totalSalary, employee.salary);
+
+        employees[employeeId].salary = SafeMath.mul(salary, 1 ether);
+        employees[employeeId].lastPayday = now;
+
+        totalSalary = SafeMath.add (totalSalary, employee.salary);
+    }
+
+    function addFund() public payable returns (uint) {
+        return this.balance;
+    }
+
+    function calculateRunway() public view returns (uint) {
+        return SafeMath.div(this.balance, totalSalary);
+    }
+
+    function hasEnoughFund() private view returns (bool) { return calculateRunway() > 0; }
+
+
+    function getPaid() employeeExist(msg.sender) public {
+        var employee = employees[msg.sender];
+        uint nextPayday = SafeMath.add(employee.lastPayday, PAY_DURATION);
+        assert(nextPayday < now);
+        employees[msg.sender].lastPayday = nextPayday;
+        employee.id.transfer(employee.salary);
+    }
+
+    function checkEmployee (address employeeId) internal view returns (uint salary, address id, uint payDay) {
+        var employee = employees[employeeId];
+        return (employee.salary, employee.id, employee.lastPayday);
+    }
+
+    function changePaymentAddress (address oldEmployeeId, address newEmployeeId) onlyOwner employeeExist(oldEmployeeId) employeeNotExist(newEmployeeId) public {
+        var oldEmployee = employees[oldEmployeeId];
+        _partialPaid (oldEmployee);
+        employees[newEmployeeId] = Employee(newEmployeeId, SafeMath.mul(oldEmployee.salary, 1 ether), now);
+        delete employees[oldEmployeeId];
+    }
+}
